40c85c42ddf415d41712fc22a9b1ff1c687318b2
lonefox <96380261+lonefox-xxx@users.noreply.github.com>
2024-02-04 10:20:24 +0530

    function cleaned]

--- utils/addUploadlogs.js
+++ utils/addUploadlogs.js
@@ -3,7 +3,6 @@ const db = new Database()
 
 async function addUploadlogs(id, preview, title, teleURL, siteURL) {
     const sddres = await db.addLogs({ id, preview, title, teleURL, siteURL }, 'uploadLogs')
-    console.log(sddres)
     return 'ok'
 }
 
