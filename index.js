929ef39e50cea3972dc00f8629bca0e680d9cbba
lonefox <96380261+lonefox-xxx@users.noreply.github.com>
2024-02-04 12:03:23 +0530

    fetching post added

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ require('dotenv').config({ path: './.env' })
 require('./database/redis')
 require('./database/mongodb')
 const express = require('express')
-const { MongoClient } = require('mongodb')
 const app = express()
 const port = process.env.PORT || 3000
 const processPages = require('./main')
 const path = require('path')
 const ConvertandSendInitialPhotos = require('./helper/sendinitialphotos')
 const handleAutoUpload = require('./crons/handleAutoupload')
+const Database = require('./database/database')
+const { default: axios } = require('axios')
+const db = new Database()
+const BOT_TOKEN = process.env.BOT_TOKEN
+const cors = require('cors')
+
+app.use(cors({
+    origin: '*'
+}))
 
 app.get('/', (req, res) => res.send('everything ok'))
 app.get('/start', (req, res) => {
@@ -32,10 +40,41 @@ app.get('/startconvertandsendinitialphotos', (req, res) => {
     }
 })
 
+app.get('/getphoto', async (req, res) => {
+    const { id } = req.query
+    if (!id) return res.send({ success: false, msg: 'on item is specified!' })
 
+    const { data } = await db.getLogs({ id }, 'photos')
+    if (data.length <= 0) return res.send({ success: false, msg: `Post can't find` })
+    const dataUrl = `https://api.telegram.org/file/bot${BOT_TOKEN}/${data[0].photostorageData.file}`
+    const { data: postData } = await axios.get(dataUrl)
+
+    res.send({ success: true, msg: 'success', postData })
+})
 // Cron jobs
 handleAutoUpload()
 
 app.listen(port, () => {
     console.log(`Server running on port : ${port}!`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
